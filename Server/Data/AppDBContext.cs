f704eccdd6666ac14945eba047fe86f89b687fda
Jacek Szczepański <jacogitproject@gmail.com>
2022-01-29 23:17:50 +0100

    Updating  the database (1/2)

--- Server/Data/AppDBContext.cs
+++ Server/Data/AppDBContext.cs
@@ -6,12 +6,16 @@ namespace Server.Data
     public class AppDBContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
 
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            #region Categories seed
+
 
             Category[] categoriesToSeed = new Category[3];
 
@@ -28,6 +32,71 @@ namespace Server.Data
             }
 
             modelBuilder.Entity<Category>().HasData(categoriesToSeed);
+            #endregion
+
+
+
+            modelBuilder.Entity<Post>(
+                entity =>
+                {
+                    entity.HasOne(post => post.Category)
+                    .WithMany(category => category.Posts)
+                    .HasForeignKey("CategoryId");
+                });
+
+            #region Posts seed
+            Post[] postsToSeed = new Post[6];
+
+            for (int i = 0; i < 7; i++)
+            {
+                string postTitle = string.Empty;
+                int categoryId = 0;
+                switch (i)
+                {
+                    case 1:
+                        postTitle = "First post";
+                        categoryId = 1;
+                        break;
+                    case 2:
+                        postTitle = "Second post";
+                        categoryId = 2;
+                        break;
+                    case 3:
+                        postTitle = "Third post";
+                        categoryId = 3;
+                        break;
+                    case 4:
+                        postTitle = "Fourth post";
+                        categoryId = 4;
+                        break;
+                    case 5:
+                        postTitle = "Fifth post";
+                        categoryId = 5;
+                        break;
+                    case 6:
+                        postTitle = "Sixth post";
+                        categoryId = 6;
+                        break;
+
+                    default:
+                        break;
+                }
+                postsToSeed[i - 1] = new Post
+                {
+                    PostId = i,
+                    ThumbnailimagePath = "uploads/placeholder.jpg",
+                    Title = postTitle,
+                    Excerpt = $"This is the excerpt for post {i}. An excerpt is a little extraction from a larger piece of text. Sort of lika a preview ",
+                    Content = String.Empty,
+                    PublishDate = DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm"),
+                    Author = "John Doe",
+                    CategoryId = categoryId
+                };
+            }
+
+            modelBuilder.Entity<Post>().HasData(postsToSeed);
+            #endregion
+
         }
 
     }
