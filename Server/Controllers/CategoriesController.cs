a113e10d278047676218fc6f1a9e86254374e7dc
Jacek Szczepański <jacogitproject@gmail.com>
2022-01-30 00:56:21 +0100

    Expending the categories controller (1/2)

--- Server/Controllers/CategoriesController.cs
+++ Server/Controllers/CategoriesController.cs
@@ -15,6 +15,7 @@ namespace Server.Controllers
         {
             _appDBContext = appDBContext;
         }
+        #region CRUD operations
 
         [HttpGet]
         public async Task<IActionResult> Get()
@@ -22,5 +23,66 @@ namespace Server.Controllers
             var categories = await _appDBContext.Categories.ToListAsync();
             return Ok(categories);
         }
+
+        //website.com/api/categories/withposts
+        [HttpGet("withposts")]
+        public async Task<IActionResult> GetWithPosts()
+        {
+            List<Category> categories = await _appDBContext.Categories
+                .Include(category => category.Posts)
+                .ToListAsync();
+
+            return Ok(categories);
+        }
+
+        //website.com/api/categories/3
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var category = await GetCategoryByCategoryId(id, false);
+            return Ok(category);
+        }
+
+        [HttpGet("withposts/{id}")]
+        public async Task<IActionResult> GetWithPosts(int id)
+        {
+            var category = await GetCategoryByCategoryId(id, true);
+            return Ok(category);
+        }
+
+
+        #endregion
+
+        #region Utility methods
+
+        [NonAction]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private async Task<bool> PersistChangesToDatabase()
+        {
+            var amountOfChanges = await _appDBContext.SaveChangesAsync();
+            return amountOfChanges > 0;
+        }
+
+        [NonAction]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private async Task<Category> GetCategoryByCategoryId(int categoryId, bool withPosts)
+        {
+            Category categoryToGet = null;
+            if (withPosts == true)
+            {
+                categoryToGet = await _appDBContext.Categories
+                    .Include(category => category.Posts)
+                    .FirstAsync(category => category.CategoryId == categoryId);
+            }
+            else
+            {
+                categoryToGet = await _appDBContext.Categories
+                    .FirstAsync(category => category.CategoryId == categoryId);
+            }
+
+            return categoryToGet;
+        }
+
+        #endregion
     }
 }
